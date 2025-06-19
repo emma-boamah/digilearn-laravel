2207d850f83b0ae740cfa1c9c508d0024fc35280
emma-boamah <emmanuelboamah046@gmail.com>
2025-06-19 00:52:15 +0000

    version 3.0.2 of shoutoutgh

--- digilearn-laravel/resources/views/dashboard/lesson-view.blade.php
+++ digilearn-laravel/resources/views/dashboard/lesson-view.blade.php
@@ -1401,7 +1401,6 @@
             
             <div class="sidebar-logo">
                 <img src="{{ asset('images/shoutoutgh-logo.png') }}" alt="ShoutOutGh">
-                <span class="sidebar-brand">ShoutOutGh</span>
             </div>
         </div>
         
