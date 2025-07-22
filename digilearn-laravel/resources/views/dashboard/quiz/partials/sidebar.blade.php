d4078e86b9d6aa09a4dbba3f5f8c72aa44a5bf9b
emma-boamah <emmanuelboamah046@gmail.com>
2025-07-22 16:18:21 +0000

    added some routes to the quizzes sidebar

--- digilearn-laravel/resources/views/dashboard/quiz/partials/sidebar.blade.php
+++ digilearn-laravel/resources/views/dashboard/quiz/partials/sidebar.blade.php
@@ -40,7 +40,7 @@
 
         <div class="sidebar-section">
             <div class="sidebar-section-title">Learning</div>
-            <a href="/my-progress" class="sidebar-menu-item">
+            <a href="{{ route('dashboard.my-progress') }}" class="sidebar-menu-item">
                 <svg class="sidebar-menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
                 </svg>
@@ -53,6 +53,13 @@
                 </svg>
                 <span class="sidebar-menu-text">Saved Lessons</span>
                 <div class="tooltip">Saved Lessons</div>
+            </a>
+            <a href="{{ route('dashboard.my-projects') }}" class="sidebar-menu-item">
+                <svg class="sidebar-menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
+                </svg>
+                <span class="sidebar-menu-text">My Projects</span>
+                <div class="tooltip">My Projects</div>
             </a>
             <a href="{{ route('dashboard.notes') }}" class="sidebar-menu-item">
                 <svg class="sidebar-menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
