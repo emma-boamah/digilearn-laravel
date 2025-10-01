493f8c9826839d9ad1dbe13b08a614f9a7d496da
emma-boamah <emmanuelboamah046@gmail.com>
2025-10-01 15:28:47 +0000

    modified the cache storage system

--- digilearn-laravel/config/database.php
+++ digilearn-laravel/config/database.php
@@ -143,7 +143,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
+        'client' => env('REDIS_CLIENT', 'redis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
